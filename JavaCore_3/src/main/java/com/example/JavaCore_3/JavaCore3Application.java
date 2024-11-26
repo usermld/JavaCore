41c0e93e9d257c67fffac0053eb13913c748b307
MU <m.iouriev@gmail.com>
2024-11-26 15:58:04 +0700

    thirst homework

---
+++ JavaCore_3/src/main/java/com/example/JavaCore_3/JavaCore3Application.java
@@ -0,0 +1,13 @@
+package com.example.JavaCore_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaCore3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaCore3Application.class, args);
+	}
+
+}
---
+++ JavaCore_3/src/test/java/com/example/JavaCore_3/JavaCore3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaCore_3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaCore3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
